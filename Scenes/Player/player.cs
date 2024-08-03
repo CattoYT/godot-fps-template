e07e85e9351471b910f678a2891607d040d15d7e
CattoYT <twilsprk@gmail.com>
2024-08-03 18:50:44 +0100

    Mouse rotation

--- Scenes/Player/player.cs
+++ Scenes/Player/player.cs
@@ -1,18 +1,25 @@
 using Godot;
 using System;
+using static Godot.Mathf; 
 
 public partial class player : CharacterBody3D {
     [Export] public int Speed { get; set; } = 5;
     [Export] public float JUMPVELOCITY { get; set; } = 4.5F;
 
+    [Export] public PackedScene Bullet;
+
     [Export] public float MouseSensitivity { get; set; } = 1200;
-
-
+    
     float gravity = (float)ProjectSettings.GetSetting("physics/3d/default_gravity");
-
-    private Camera3D Camera;
+    
     private Vector3 _targetVelocity = Vector3.Zero;
     private Vector3 Rotation = Vector3.Zero;
+    
+    private Camera3D Camera;
+    private RayCast3D gunRay;
+    private MeshInstance3D playerMesh;
+    
+    
 
 
 
@@ -20,11 +27,11 @@ public partial class player : CharacterBody3D {
 
 
     public override void _Ready() {
-        var gunRay = GetNode<RayCast3D>("Head/Camera3d/RayCast3D");
+        gunRay = GetNode<RayCast3D>("Head/Camera3d/RayCast3d");
         Camera = GetNode<Camera3D>("Head/Camera3d");
-
+        playerMesh = GetNode<MeshInstance3D>("MeshInstance3d");
         gunRay.AddException(this);
-
+        Bullet = ResourceLoader.Load<PackedScene>("res://Scenes/Bullet/Bullet.tscn");
         Input.MouseMode = Input.MouseModeEnum.Captured;
     }
 
@@ -45,11 +52,11 @@ public partial class player : CharacterBody3D {
         }
 
         if (Input.IsActionJustPressed("Shoot")) {
-            //Shoot();
+            shoot();
         }
 
         Vector2 inputDirection = Input.GetVector("moveLeft", "moveRight", "moveUp", "moveDown");
-        var direction = (Transform.Basis * new Vector3(inputDirection.X, 0, inputDirection.Y)).Normalized();
+        var direction = (Camera.Transform.Basis * new Vector3(inputDirection.X, 0, inputDirection.Y)).Normalized();
         if (direction != Vector3.Zero) {
             _targetVelocity.X = direction.X * Speed;
             _targetVelocity.Z = direction.Z * Speed;
@@ -57,8 +64,10 @@ public partial class player : CharacterBody3D {
         else {
             //unsure of this being necessary
             // source: line 37 and 38 in https://github.com/Shidoengie/FPS-template-gd4/blob/main/Scenes/Player/player.gd
-            _targetVelocity = Vector3.Zero;
+            _targetVelocity.X = 0;
+            _targetVelocity.Z = 0;
         }
+        
         Velocity = _targetVelocity;
         MoveAndSlide();
 
@@ -68,12 +77,32 @@ public partial class player : CharacterBody3D {
 
     }
 
-    // public override void _Input(InputEvent @event) {
-    //     if (@event is InputEventMouseMotion MMEvent) {
-    //         // unrelated, but c# syntax is really nice, i didnt have to google this one
-    //         Rotation.Y -= MMEvent.Relative.X / MouseSensitivity;
-    //         //Camera.Rotation.X = MMEvent.Relative.Y / MouseSensitivity;
-    //
-    //     }
-    // }
+    public override void _Input(InputEvent @event) {
+        if (@event is InputEventMouseMotion MMEvent) {
+            // unrelated, but c# syntax is really nice, i didnt have to google this one
+            Rotation.Y -=MMEvent.Relative.X / MouseSensitivity;
+            Rotation.X -= MMEvent.Relative.Y / MouseSensitivity;
+            Rotation.X = Clamp(Rotation.X, DegToRad(-90), DegToRad(90));
+            Camera.Rotation = Rotation;
+            playerMesh.Rotation = new Vector3(Rotation.X, 0, 0);
+            
+    
+        }
+    }
+
+    private void shoot() {
+        if (!gunRay.IsColliding()) {
+            return;
+        }
+
+        var BulletInstance = (Node3D)Bullet.Instantiate();
+
+        BulletInstance.TopLevel = true;
+        GetParent().AddChild(BulletInstance);
+        BulletInstance.Position = gunRay.GetCollisionPoint(); //test, unsure
+        BulletInstance.LookAt((gunRay.GetCollisionPoint()+gunRay.GetCollisionPoint()), Vector3.Back);
+        GD.Print(gunRay.GetCollisionPoint());
+        GD.Print(gunRay.GetCollisionPoint()+gunRay.GetCollisionNormal());
+
+    }
 }
